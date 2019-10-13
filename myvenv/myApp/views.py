52fbe41b15da2bdfd8046b6d4ba292f37f31a9ee
HarshiSharma <harshi0901@gmail.com>
2019-10-13 11:50:18 +0530

    Initial commit

---
+++ myvenv/myApp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    context = {"var": 1243}
+    return render(request, "myApp/home.html", context)
